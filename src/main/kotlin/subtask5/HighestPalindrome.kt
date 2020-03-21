a526175440c4de8ecc2eb8373f63734dad768033
Artem Yakovlev <13.zrka@gmail.com>
2020-03-21 22:10:26 +0300

    fet(tasks): All tasks done

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -1,9 +1,51 @@
 package subtask5
 
+import java.lang.Integer.max
+import java.lang.StringBuilder
+
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val editable = StringBuilder(digitString)
+        val needToChange = BooleanArray(n / 2)
+        var numberChanges = k
+
+        for (i in 0 until editable.length / 2) {
+            if (editable[i] != editable[n - 1 - i]) {
+                numberChanges--
+                needToChange[i] = true
+            }
+        }
+
+        if (numberChanges >= 0) {
+            for (i in 0 until editable.length / 2) {
+                if (!needToChange[i]) {
+                    if (numberChanges >= 2 && editable[i] != '9') {
+                        equalize(editable, i, '9')
+                        numberChanges -= 2
+                    }
+                } else {
+                    if (editable[i] != '9' && editable[n - i - 1] != '9' && numberChanges >= 1) {
+                        equalize(editable, i, '9')
+                        numberChanges--
+                    } else {
+                        equalize(editable, i,
+                            max(editable[i].toInt(), editable[n - i - 1].toInt()).toChar())
+                    }
+                }
+            }
+            if (numberChanges >= 1) {
+                editable[n / 2] = '9'
+            }
+            return editable.toString()
+        } else {
+            return "-1"
+        }
+    }
+
+    private fun equalize(editable: StringBuilder, index: Int, value: Char) {
+        editable[index] = value
+        editable[editable.length - 1 - index] = value
     }
 }
